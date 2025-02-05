8693c7ff067111bd3610894629db1c60b92a8d13
luiscggonzalez <redes.luisgonzalez@gmail.com>
2025-02-05 11:31:04 -0400

    Arreglo de Postgress y agregado función de eliminar invoices v2

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -21,25 +21,21 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
 
-        const { customerId, amount, status } = CreateInvoice.parse({
-            customerId: formData.get('customerId'),
-            amount: formData.get('amount'),
-            status: formData.get('status'),
-        });
-        const amountInCents = amount * 100;
-        const date = new Date().toISOString().split('T')[0];
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
 
-        await sql`
-            INSERT INTO invoices (customer_id, amount, status, date)
-            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-        `;
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
 
-        revalidatePath('/dashboard/invoices');
-        redirect('/dashboard/invoices');
-    } catch (error) {
-        console.log(error)
-        message.error('Hubo un error al crear la factura');
-    }
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
