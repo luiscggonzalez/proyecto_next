9165450d50cf553863fc0ab2b028543c8d9345fa
luiscggonzalez <redes.luisgonzalez@gmail.com>
2025-02-05 11:38:17 -0400

    Arreglo de Postgress y agregado función de eliminar invoices v3

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -11,7 +11,7 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string(),
-    amount: z.string(),
+    amount: z.coerce.number(),
     status: z.enum(['pending', 'paid']),
     date: z.string(),
 });
