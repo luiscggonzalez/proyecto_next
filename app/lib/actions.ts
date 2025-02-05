9cdefdb81d0f192acf52ea9ffd7cd95d0e46c3db
luiscggonzalez <redes.luisgonzalez@gmail.com>
2025-02-05 11:29:10 -0400

    Arreglo de Postgress y agregado función de eliminar invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,13 +1,12 @@
 'use server';
 
 import { z } from 'zod';
+import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import postgres from 'postgres';
+
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
-const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 const FormSchema = z.object({
     id: z.string(),
@@ -18,21 +17,51 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+
+        const { customerId, amount, status } = CreateInvoice.parse({
+            customerId: formData.get('customerId'),
+            amount: formData.get('amount'),
+            status: formData.get('status'),
+        });
+        const amountInCents = amount * 100;
+        const date = new Date().toISOString().split('T')[0];
+
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+
+        revalidatePath('/dashboard/invoices');
+        redirect('/dashboard/invoices');
+    } catch (error) {
+        console.log(error)
+        message.error('Hubo un error al crear la factura');
+    }
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
     const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
 
     await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
 }
