2398856fcde8157d41cc5b2bdc3636843326176e
luiscggonzalez <redes.luisgonzalez@gmail.com>
2025-02-06 13:37:48 -0400

    Arreglo de algunos código mal colocado y agregado de metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: {
+        template: '%s | App prueba Luis',
+        default: 'App prueba de Luis',
+    },
+    description: 'Esta es una aplicación web de prueba',
+    metadataBase: new URL('https://proyecto-next-git-luisqa-luis-projects-d7ab4804.vercel.app'),
+};
 
 export default function RootLayout({
   children,
